cfee3531c66fd8d9664e361ee90cdb79731df105
simotark <simostarkiainen@gmail.com>
2023-01-03 23:48:40 +0200

    terraform formatting

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 # provides configuration detauls for terraform
 terraform {
-    required_providers {
-      azurerm = {
-        source = "hashicorp/azurerm"
-        version = "~>2.31.1" #pessimistic operator; prevents major version change
-      }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.31.1" #pessimistic operator; prevents major version change
     }
+  }
 }
 
 # this tidbit needs to exist, lest an error occurreth 
@@ -14,10 +14,11 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-    name = "my-resource-group"
-    location = "northeurope"
-    tags = {
-        environment = "dev"
-        source = "Terraform"
-    }
+  name     = "my-resource-group"
+  location = "northeurope"
+  tags = {
+    environment = "dev"
+    source      = "Terraform"
+    owner       = "simo"
+  }
 }
