7a4d8de9d07870dbfe54d5e6cb3f0791176cd20d
simotark <simostarkiainen@gmail.com>
2023-02-05 22:22:07 +0200

    add virtual machine

--- main.tf
+++ main.tf
@@ -38,6 +38,39 @@ resource "azurerm_virtual_network" "vnet" {
   ]
 }
 
+resource "azurerm_subnet" "subnet" {
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  name                 = "subu-net"
+  resource_group_name  = azurerm_resource_group.rg.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "vm-nic"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  ip_configuration {
+    name                          = "just-an-ip"
+    private_ip_address_allocation = "dynamic"
+    subnet_id                     = azurerm_subnet.subnet.id
+  }
+}
+
+# barebones, but passes validation
+resource "azurerm_virtual_machine" "vm" {
+  count                 = 1
+  name                  = "Ville-Matti"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  vm_size               = "Standard_B1s"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  storage_os_disk {
+    name          = "ossi-disk"
+    create_option = "FromImage"
+  }
+}
+
 # works, but outputs block plan
 #resource "local_file" "output" {
 #  content  = azurerm_virtual_network.vnet.name
